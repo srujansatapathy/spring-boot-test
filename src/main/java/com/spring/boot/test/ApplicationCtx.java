b3708d3865b85e25d3eee23bc58296ad4407f00e
Srujan <srujansatapathy@gmail.com>
2016-08-29 10:10:42 -0400

    Base Spring Boot Project

---
+++ src/main/java/com/spring/boot/test/ApplicationCtx.java
@@ -0,0 +1,24 @@
+package com.spring.boot.test;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class ApplicationCtx {
+	
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(ApplicationCtx.class, args);
+        
+        System.out.println("Beans provided by Spring Boot");
+        
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+    }
+
+}
